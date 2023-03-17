be98b86f0cef7a8ade6f5b9fcdbf28dfeb11530b
AndresdoSantos <andres.dosantosbritoamaral@gmail.com>
2023-03-17 16:58:35 -0300

    feat: configure knex & sqlite

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
 import fastify from 'fastify'
 
+import { knex } from './database'
+
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'Hello World'
+app.get('/hello', async () => {
+  console.log('render')
+
+  const tables = await knex('sqlite_schema').select('*')
+
+  console.log('render 2')
+
+  return tables
 })
 
 app
